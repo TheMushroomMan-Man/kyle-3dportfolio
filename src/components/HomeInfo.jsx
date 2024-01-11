d01ef3f6bd63ff6557a82626899416d5e998c7b7
Kyle Molyneux <kylemolyneux13@gmail.com>
2024-01-11 10:40:16 +0200

    Initial commit

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,66 @@
+import { Link } from 'react-router-dom';
+
+import { arrow } from '../assets/icons';
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        Hi, I'm
+        <span className="font-semibold mx-2 text-white">Kyle</span>
+        👋
+        <br />A Front-End Developer from George, South Africa
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          Acquired diverse skills through courses, tutorials, and projects,
+          despite lacking direct professional experience in the field.
+        </p>
+
+        <Link to="/about" className="neo-brutalism-white neo-btn">
+          Learn more
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className="info-box">
+        <p className="font-medium text-center sm:text-xl">
+          See a selection of projects that showcase my skills and capabilities.
+        </p>
+
+        <Link to="/projects" className="neo-brutalism-white neo-btn">
+          Visit my portfolio
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          Need a project done or looking for a dev? <br /> I'm just a few
+          keystrokes away
+        </p>
+
+        <Link to="/contact" className="neo-brutalism-white neo-btn">
+          Let's talk
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
